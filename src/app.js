dc154ac58498ca8e9e8b88e2cb5bdda62d604cb2
Robson Mendonça <rob.ams@gmail.com>
2020-08-13 22:22:09 -0300

    Cors autorização.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD");
+  res.header("Access-Control-Request-Method", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  next();
+})
+
+
 app.use('/',require('./routes'))
 
 module.exports = app
